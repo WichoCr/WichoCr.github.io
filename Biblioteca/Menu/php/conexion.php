767c7c8094815ad158584de0b402dc04378b3065
WichoCr <suarezcruzluis48@gmail.com>
2025-04-11 09:33:50 -0600

    Delete Biblioteca directory

--- Biblioteca/Menu/php/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "biblioteca");
-
-if (!$conn) {
-    die("Error de conexión: " . mysqli_connect_error());
-}
-?>
--- Biblioteca/login/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "biblioteca");
-
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
-?>
